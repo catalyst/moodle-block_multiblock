b813697c7d61f5cf4191e66813a7a4f7c43dc953
Arantor <sleeping@myperch.org>
2020-02-13 21:30:49 +0000

    Added vertical-tabbed (right) layout.

--- changelog.php
+++ changelog.php
@@ -29,6 +29,10 @@ defined('MOODLE_INTERNAL') || die();
 Multiblock Changelog.
 (File protected as a .php file to avoid leaking details of instance in use.)
 
+1.0.3 - 2020021102
+ * Added vertical tabs - right aligned. (#33)
+
+
 1.0.2 - 2020021101
  * Added a changelog.
  * Added badges to the readme.
